534c2f20b8bc0e26d680a4fd909505703c81f54e
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-02-21 17:52:28 +0100

    Add interfaces for new components

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,5 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
 enum Status {
   Pending = "Pending",
   Approved = "Approved",
@@ -48,4 +50,42 @@ export interface Location {
   zip: string;
   country: string;
   name: string;
+}
+
+export interface MenuEntry {
+  name: string;
+  icon: IconDefinition;
+  url: string;
+  method: string;
+}
+
+export interface Field {
+  name: string;
+  type:
+    | "text"
+    | "password"
+    | "checkbox"
+    | "radio"
+    | "number"
+    | "date"
+    | "time"
+    | "datetime-local"
+    | "email"
+    | "search"
+    | "tel"
+    | "url"
+    | "color";
+  desc?: string;
+  logic?: () => Promise<{ value: string; name: string }[]>;
+  required?: boolean;
+  value?: string;
+  entries?: { value: string; name: string }[];
+}
+
+export interface SpecialField extends Omit<Field, "type"> {
+  type: "select" | "info" | "checkbox";
+}
+
+export interface CreateOpts extends RequestInit {
+  endpoint: string;
 }
