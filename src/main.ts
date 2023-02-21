bc5e4d8c3e261744beddea13e3df43d986003670
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-02-21 17:52:13 +0100

    Add exports

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
+import "./main.css";
+
+/** components */
 import LMSCard from "./components/LMSCard";
 import LMSEventsFilter from "./components/LMSEventsFilter";
+import LMSFloatingMenu from "./components/LMSFloatingMenu";
+import LMSModal from "./components/LMSModal";
 
+/** extensions */
+import LMSEventMangementMenu from "./extensions/LMSEventManagementMenu";
+import LMSEventTypesModal from "./extensions/LMSEventTypesModal";
+import LMSEventTypesTable from "./extensions/LMSEventTypesTable";
+
+/** views */
 import LMSEventsView from "./views/LMSEventsView";
-
-import "./main.css";
+import StaffEventTypesView from "./views/StaffEventTypesView";
+import StaffSettingsView from "./views/StaffSettingsView";
 
 export default {
   LMSCard,
   LMSEventsFilter,
+  LMSFloatingMenu,
+  LMSModal,
+  LMSEventMangementMenu,
+  LMSEventTypesModal,
+  LMSEventTypesTable,
   LMSEventsView,
+  StaffEventTypesView,
+  StaffSettingsView,
 };
