44c98b35bccd96373fbd5dcbc6310aa4664454e8
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-02-22 17:01:41 +0100

    Make settings comonent more verbose

--- src/views/StaffSettingsView.ts
+++ src/views/StaffSettingsView.ts
@@ -1,9 +1,13 @@
+import { bootstrapStyles } from "@granite-elements/granite-lit-bootstrap/granite-lit-bootstrap-min.js";
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
 
 @customElement("lms-staff-settings-view")
 export default class StaffSettingsView extends LitElement {
+
+  static override styles = [bootstrapStyles]
+
   override render() {
-    return html`<h1>Not implemented!</h1>`;
+    return html`<h1 class="text-center">Not implemented!</h1>`;
   }
 }
