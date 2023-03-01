4a98808aaf12505127af90b40e4173e64522d712
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-03-01 15:20:41 +0100

    Add uuid based state tracking inside the card deck
    For whomever's wondering. The bootstrap styles don't match up if we extract the cards
    themselves into separate components.

--- src/components/LMSStaffEventCardDeck.ts
+++ src/components/LMSStaffEventCardDeck.ts
@@ -1,21 +1,36 @@
 import { bootstrapStyles } from "@granite-elements/granite-lit-bootstrap/granite-lit-bootstrap-min.js";
-import { LitElement, html, TemplateResult } from "lit";
+import { LitElement, html, TemplateResult, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import LMSStaffEventCardForm from "./LMSStaffEventCardForm";
-import { Column, TargetGroup } from "../interfaces";
+import LMSStaffEventCardForm from "./LMSStaffEventCard/LMSStaffEventCardForm";
+import { Column } from "../interfaces";
 import { InputType } from "../types";
+import LMSStaffEventCardAttendees from "./LMSStaffEventCard/LMSStaffEventCardAttendees";
+import LMSStaffEventCardPreview from "./LMSStaffEventCard/LMSStaffEventCardPreview";
+import TemplateResultConverter from "../lib/TemplateResultConverter";
 
 declare global {
   interface HTMLElementTagNameMap {
     "lms-staff-event-card-form": LMSStaffEventCardForm;
+    "lms-staff-event-card-attendees": LMSStaffEventCardAttendees;
+    "lms-staff-event-card-preview": LMSStaffEventCardPreview;
   }
 }
 
 @customElement("lms-staff-event-card-deck")
 export default class LMSStaffEventCardDeck extends LitElement {
   @property({ type: Array }) data: Column[] = [];
+  @property({ type: Object }) cardStates: Map<string, string[]> = new Map();
 
-  static override styles = [bootstrapStyles];
+  static override styles = [
+    bootstrapStyles,
+    css`
+      .card-deck {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr));
+        grid-gap: 0.5rem;
+      }
+    `,
+  ];
 
   override connectedCallback() {
     super.connectedCallback();
@@ -38,18 +53,36 @@ export default class LMSStaffEventCardDeck extends LitElement {
        *  and remember that you have to resolve the Promise returned by
        *  the previous call before you can continue with the next one. */
       .then(async (result) => {
+        /** Here we initialize the card states so we can track them
+         *  individually going forward. */
+        result.forEach(() => {
+          this.cardStates.set(
+            crypto.getRandomValues(new Uint32Array(2)).join("-"),
+            ["data"]
+          );
+        });
+
         const data = await Promise.all(
-          result.map(async (target_group: TargetGroup) => {
-            const entries = await Promise.all(
-              Object.entries(target_group).map(async ([name, value]) => [
-                name,
-                await this.getInputFromColumn({ name, value }),
-              ])
-            );
-            return Object.fromEntries(entries);
-          })
+          result.map(
+            async (promisedTemplateResult: Promise<TemplateResult>) => {
+              const entries = await Promise.all(
+                Object.entries(promisedTemplateResult).map(
+                  async ([name, value]) => [
+                    name,
+                    await this.getInputFromColumn({ name, value }),
+                  ]
+                )
+              );
+              return Object.fromEntries(entries);
+            }
+          )
         );
-        this.data = data;
+        /** Here we tag every datum with the uuid we generated earlier. */
+        const cardStatesArray = Array.from(this.cardStates);
+        this.data = data.map((datum, index) => ({
+          ...datum,
+          uuid: cardStatesArray[index][0],
+        }));
       })
       .catch((error) => {
         console.error(error);
@@ -259,6 +292,24 @@ ${value}</textarea
     return inputs.get(name) ? inputs.get(name)!() : inputs.get("default")!();
   }
 
+  private handleTabClick(event: Event) {
+    event.preventDefault();
+    const target = event.target as HTMLElement;
+    const tab = target.closest("li");
+    if (!tab) return;
+
+    const previousActiveTab = tab.closest("ul")?.querySelector(".active");
+    if (!previousActiveTab) return;
+    previousActiveTab.classList.remove("active");
+
+    tab.firstElementChild?.classList.add("active");
+
+    const { content, uuid } = tab.dataset;
+    if (!content || !uuid) return;
+    this.cardStates.set(uuid, [content]);
+    this.requestUpdate();
+  }
+
   override render() {
     return html`
       <div class="container-fluid mx-0">
@@ -268,25 +319,65 @@ ${value}</textarea
               <div class="card">
                 <div class="card-header">
                   <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item">
+                    <li
+                      class="nav-item"
+                      data-content="data"
+                      data-uuid=${datum.uuid}
+                      @click=${this.handleTabClick}
+                    >
                       <a class="nav-link active" href="#">Data</a>
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Attendees</a>
+                    <li
+                      class="nav-item"
+                      data-content="attendees"
+                      data-uuid=${datum.uuid}
+                      @click=${this.handleTabClick}
+                    >
+                      <a
+                        class="nav-link"
+                        href="#"
+                        style="text-decoration: line-through;"
+                        >Attendees</a
+                      >
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">???</a>
-                    </li>
-                    <li class="nav-item">
+                    <li
+                      class="nav-item"
+                      data-content="preview"
+                      data-uuid=${datum.uuid}
+                      @click=${this.handleTabClick}
+                    >
                       <a class="nav-link">Preview</a>
                     </li>
                   </ul>
                 </div>
                 <div class="card-body">
-                  <h5 class="card-title">${datum.name}</h5>
+                  <h5 class="card-title">
+                    ${html`<span class="badge badge-primary"
+                      >${[
+                        new TemplateResultConverter(
+                          datum.name
+                        ).getRenderValues(),
+                      ]}</span
+                    >`}
+                  </h5>
                   <lms-staff-event-card-form
                     .datum=${datum}
+                    ?hidden=${!(
+                      this.cardStates?.get(datum.uuid as string)?.[0] === "data"
+                    )}
                   ></lms-staff-event-card-form>
+                  <lms-staff-event-card-attendees
+                    ?hidden=${!(
+                      this.cardStates?.get(datum.uuid as string)?.[0] ===
+                      "attendees"
+                    )}
+                  ></lms-staff-event-card-attendees>
+                  <lms-staff-event-card-preview
+                    ?hidden=${!(
+                      this.cardStates?.get(datum.uuid as string)?.[0] ===
+                      "preview"
+                    )}
+                  ></lms-staff-event-card-preview>
                 </div>
               </div>
             `
