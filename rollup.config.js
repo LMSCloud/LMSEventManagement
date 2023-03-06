72c79dd4c4336c5b254548c33e24edc83cf7858d
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-03-06 13:57:29 +0100

    Add analyzer plugin to track bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
 import autoprefixer from "autoprefixer";
+import analyze from "rollup-plugin-analyzer";
 import path from "path";
 
 export default {
@@ -26,6 +27,7 @@ export default {
       minimize: true,
       sourceMap: true,
     }),
-    typescript()
+    typescript(),
+    analyze(),
   ],
 };
