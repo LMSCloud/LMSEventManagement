5e604e288a6966b7ef89217451ef5978230f3fb8
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-04-20 17:19:30 +0200

    Add dynamic header values for xgettext extraction

---
+++ src/dynamically_added_strings.ts
@@ -0,0 +1,24 @@
+/** Dynamically added Strings */
+
+/** This file is used to provide an entry point to xgettext for strings that come straight from the database
+ *  which you are translated by running the gettext function on runtime values. Works reliably. */
+
+const __ = (string: string) => string;
+
+__("id");
+__("name");
+__("min_age");
+__("max_age");
+__("street");
+__("number");
+__("city");
+__("zip");
+__("country");
+__("target_groups");
+__("min_age");
+__("max_age");
+__("max_participants");
+__("location");
+__("image");
+__("description");
+__("open_registration");
