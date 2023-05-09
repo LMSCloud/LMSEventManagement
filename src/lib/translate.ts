8c96deee27d5ce5646067eb928ac2ed0e74f9075
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-05-09 21:13:29 +0200

    Add attr__  directive for attribute/property translations

--- src/lib/translate.ts
+++ src/lib/translate.ts
@@ -1,5 +1,12 @@
 import Gettext from "gettext.js";
-import { Directive, PartInfo, directive } from "lit/directive.js";
+import {
+  AttributePart,
+  Directive,
+  PartInfo,
+  PartType,
+  PropertyPart,
+  directive,
+} from "lit/directive.js";
 import { Part } from "lit/html.js";
 
 let i18nInstance: Gettext.Gettext;
@@ -38,6 +45,11 @@ async function loadTranslations(
   }
 }
 
+/**
+ * TranslateDirective is used for translating text content within Lit templates.
+ * It fetches translations from an external source and automatically updates the DOM.
+ * Usage: html`<div>${__(text)}</div>`
+ */
 export class TranslateDirective extends Directive {
   private _element: HTMLElement | null = null;
   private _textNode: Text | null = null;
@@ -84,6 +96,11 @@ export class TranslateDirective extends Directive {
     }
   }
 
+  private generatePlaceholder() {
+    const rng = Math.floor(Math.random() * 5) + 8;
+    return "\u00A0".repeat(rng);
+  }
+
   override update(_part: Part, [text]: Parameters<this["render"]>) {
     this.____(text).then((translatedText) => {
       this.updateTranslation(text);
@@ -97,8 +114,7 @@ export class TranslateDirective extends Directive {
       if (translationsLoaded || this._locale.startsWith("en")) {
         this._textNode.textContent = _text;
       } else {
-        const rng = Math.floor(Math.random() * 10) + 4;
-        this._textNode.textContent = "\u00A0".repeat(rng);
+        this._textNode.textContent = this.generatePlaceholder();
         this._element.classList.add(
           "skeleton",
           "skeleton-text",
@@ -111,6 +127,80 @@ export class TranslateDirective extends Directive {
   }
 }
 
+/**
+ * TranslateAttributeDirective is used for translating attribute values within Lit templates.
+ * It fetches translations from an external source and automatically updates the DOM.
+ * Usage: html`<div title=${attr__(title)}></div>`
+ */
+export class TranslateAttributeDirective extends Directive {
+  private _element: HTMLElement | null = null;
+  private _name: string | null = null;
+  private _locale: string;
+
+  constructor(partInfo: PartInfo) {
+    super(partInfo);
+
+    this._locale = document.documentElement.lang.slice(0, 2);
+    if (
+      partInfo.type !== PartType.PROPERTY &&
+      partInfo.type !== PartType.ATTRIBUTE
+    ) {
+      throw new Error(
+        "Use of TranslateAttributeDirective on non-attribute/non-property is forbidden."
+      );
+    }
+  }
+
+  private async updateTranslation(text: string) {
+    if (translationsLoaded && this._element && this._name) {
+      const translatedText = i18nInstance.gettext(text);
+      (this._element as any)[this._name] = translatedText;
+    }
+  }
+
+  async ____(part: PropertyPart | AttributePart, text: string) {
+    this._element = part.element;
+    this._name = part.name;
+
+    if (!translationsLoaded) {
+      callbacks.push({
+        text,
+        callback: () => {
+          this.updateTranslation(text);
+          if (this._element) {
+            this._element.classList.remove("skeleton", "skeleton-text");
+          }
+        },
+      });
+
+      if (this._locale.startsWith("en")) {
+        return text;
+      } else {
+        return "";
+      }
+    } else {
+      const translatedText = i18nInstance.gettext(text);
+      return translatedText;
+    }
+  }
+
+  override update(
+    part: PropertyPart | AttributePart,
+    [text]: Parameters<this["render"]>
+  ) {
+    this.____(part, text).then((translatedText) => {
+      this.updateTranslation(translatedText);
+    });
+    return this.render(text);
+  }
+
+  render(_text: string) {
+    return _text;
+  }
+}
+
+const attr__ = directive(TranslateAttributeDirective);
+
 const __ = directive(TranslateDirective);
 
-export { __ };
+export { __, attr__ };
