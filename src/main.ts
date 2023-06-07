559770c10dc0358cf8827cf02a37e43a945f2ed7
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-06-07 22:17:03 +0200

    Resort imports and exports

--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,17 @@ import LMSSelect from "./components/inputs/modal/LMSSelect";
 
 /**   /LMSStaffEventCard */
 import LMSStaffEventCardAttendees from "./components/LMSStaffEventCard/LMSStaffEventCardAttendees";
-import LMSStaffEventCardPreview from "./components/LMSStaffEventCard/LMSStaffEventCardPreview";
-import LMSStaffEventCardForm from "./components/LMSStaffEventCard/LMSStaffEventCardForm";
 import LMSStaffEventCardsDeck from "./components/LMSStaffEventCard/LMSStaffEventCardDeck";
+import LMSStaffEventCardForm from "./components/LMSStaffEventCard/LMSStaffEventCardForm";
+import LMSStaffEventCardPreview from "./components/LMSStaffEventCard/LMSStaffEventCardPreview";
 import LMSStaffEventsFilter from "./components/LMSStaffEventCard/LMSStaffEventsFilter";
 
 /**   /LMSTable */
-import LMSTableControls from "./components/LMSTable/LMSTableControls";
-
 import LMSAnchor from "./components/LMSAnchor";
 import LMSCard from "./components/LMSCard";
 import LMSCardDetailsModal from "./components/LMSCardDetailsModal";
+import LMSContainer from "./components/LMSContainer";
+import LMSView from "./components/LMSContent";
 import LMSDropdown from "./components/LMSDropdown";
 import LMSEventsFilter from "./components/LMSEventsFilter";
 import LMSFloatingMenu from "./components/LMSFloatingMenu";
@@ -29,11 +29,12 @@ import LMSModal from "./components/LMSModal";
 import LMSPagination from "./components/LMSPagination";
 import LMSPellEditor from "./components/LMSPellEditor";
 import LMSSearch from "./components/LMSSearch";
+import LMSTableControls from "./components/LMSTable/LMSTableControls";
 import LMSToast from "./components/LMSToast";
 import LMSTooltip from "./components/LMSTooltip";
 
 /** extensions */
-import LMSEventMangementMenu from "./extensions/LMSEventManagementMenu";
+import LMSEventManagementMenu from "./extensions/LMSEventManagementMenu";
 import LMSEventsModal from "./extensions/LMSEventsModal";
 import LMSEventTypesModal from "./extensions/LMSEventTypesModal";
 import LMSEventTypesTable from "./extensions/LMSEventTypesTable";
@@ -52,42 +53,44 @@ import StaffSettingsView from "./views/StaffSettingsView";
 import StaffTargetGroupsView from "./views/StaffTargetGroupsView";
 
 export default {
-  LMSCheckboxInput,
-  LMSMatrix,
-  LMSPrimitivesInput,
-  LMSSelect,
-  LMSStaffEventCardAttendees,
-  LMSStaffEventCardPreview,
-  LMSStaffEventCardForm,
-  LMSStaffEventCardsDeck,
-  LMSStaffEventsFilter,
-  LMSTableControls,
-  LMSAnchor,
-  LMSCard,
-  LMSCardDetailsModal,
-  LMSDropdown,
-  LMSEventsFilter,
-  LMSFloatingMenu,
-  LMSImageBrowser,
-  LMSModal,
-  LMSPagination,
-  LMSPellEditor,
-  LMSSearch,
-  LMSToast,
-  LMSTooltip,
-  LMSEventMangementMenu,
-  LMSEventsModal,
-  LMSEventTypesModal,
-  LMSEventTypesTable,
-  LMSLocationsModal,
-  LMSLocationsTable,
-  LMSSettingsTable,
-  LMSTargetGroupsModal,
-  LMSTargetGroupsTable,
-  LMSEventsView,
-  StaffEventsView,
-  StaffEventTypesView,
-  StaffLocationsView,
-  StaffSettingsView,
-  StaffTargetGroupsView,
+    LMSCheckboxInput,
+    LMSMatrix,
+    LMSPrimitivesInput,
+    LMSSelect,
+    LMSStaffEventCardAttendees,
+    LMSStaffEventCardPreview,
+    LMSStaffEventCardForm,
+    LMSStaffEventCardsDeck,
+    LMSStaffEventsFilter,
+    LMSTableControls,
+    LMSAnchor,
+    LMSCard,
+    LMSCardDetailsModal,
+    LMSContainer,
+    LMSView,
+    LMSDropdown,
+    LMSEventsFilter,
+    LMSFloatingMenu,
+    LMSImageBrowser,
+    LMSModal,
+    LMSPagination,
+    LMSPellEditor,
+    LMSSearch,
+    LMSToast,
+    LMSTooltip,
+    LMSEventManagementMenu,
+    LMSEventsModal,
+    LMSEventTypesModal,
+    LMSEventTypesTable,
+    LMSLocationsModal,
+    LMSLocationsTable,
+    LMSSettingsTable,
+    LMSTargetGroupsModal,
+    LMSTargetGroupsTable,
+    LMSEventsView,
+    StaffEventsView,
+    StaffEventTypesView,
+    StaffLocationsView,
+    StaffSettingsView,
+    StaffTargetGroupsView,
 };
