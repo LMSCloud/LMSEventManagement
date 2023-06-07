6f5c3bc05f8646f2b671c7f60934226484161654
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-06-07 22:14:05 +0200

    Rough refactor of cards w/ tailwind

--- src/components/LMSCard.ts
+++ src/components/LMSCard.ts
@@ -1,92 +1,78 @@
-import { bootstrapStyles } from "@granite-elements/granite-lit-bootstrap/granite-lit-bootstrap-min.js";
 import { LitElement, css, html, nothing } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { classMap } from "lit/directives/class-map.js";
+import { tailwindStyles } from "../tailwind.lit";
 
 type Image = {
-  src: string;
-  alt: string;
+    src: string;
+    alt: string;
 };
 
 type Link = {
-  href: string;
-  text: string;
+    href: string;
+    text: string;
 };
 
 @customElement("lms-card")
 export default class LMSCard extends LitElement {
-  @property({ type: String }) override title = "";
+    @property({ type: String }) override title = "";
 
-  @property({ type: String }) text = "";
+    @property({ type: String }) text = "";
 
-  @property({ type: Object }) image = {} as Image;
+    @property({ type: Object }) image = {} as Image;
 
-  @property({ type: Array }) links = [] as Link[];
+    @property({ type: Array }) links = [] as Link[];
 
-  @property({ type: Array }) listItems = [];
+    @property({ type: Array }) listItems = [];
 
-  static override styles = [
-    bootstrapStyles,
-    css`
-      .card:hover {
-        cursor: pointer;
-        postion: relative;
-        top: -3px;
-        box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-        border: 1px solid var(--primary);
-      }
+    static override styles = [
+        tailwindStyles,
+        css`
+            svg {
+                display: inline-block;
+                width: 0.75em;
+                height: 0.75em;
+                color: #6c757d;
+            }
+        `,
+    ];
 
-      .card img {
-        aspect-ratio: 4 / 3;
-        object-fit: cover;
-      }
-
-      svg {
-        display: inline-block;
-        width: 0.75em;
-        height: 0.75em;
-        color: #6c757d;
-      }
-    `,
-  ];
-
-  override render() {
-    return html`
-      <div class="card">
-        <img
-          src=${this.image.src}
-          class="card-img-top"
-          alt=${this.image.alt}
-          ?hidden=${!this.image.src}
-        />
-        <div class="card-body">
-          <h5
-            class="card-title ${classMap({
-              "mb-0": !this.text,
-            })}"
-            ?hidden=${!this.title}
-          >
-            ${this.title}
-          </h5>
-          <p class="card-text" ?hidden=${!this.text}>${this.text}</p>
-        </div>
-        <ul
-          class="list-group list-group-flush"
-          ?hidden=${!this.listItems.length}
-        >
-          ${this.listItems.map(
-            (listItem) => html`<li class="list-group-item">${listItem}</li>`
-          )}
-        </ul>
-        <div class="card-body" ?hidden=${!this.links.length}>
-          ${this.links.length
-            ? this.links.map(
-                (link) =>
-                  html`<a href=${link.href} class="card-link">${link.text}</a>`
-              )
-            : nothing}
-        </div>
-      </div>
-    `;
-  }
+    override render() {
+        return html`
+            <div
+                class="card shadow-xl hover:relative hover:bottom-2 hover:cursor-pointer hover:shadow-2xl"
+            >
+                <figure>
+                    <img
+                        src=${this.image.src}
+                        alt=${this.image.alt}
+                        ?hidden=${!this.image.src}
+                    />
+                </figure>
+                <div class="card-body">
+                    <h5 class="card-title" ?hidden=${!this.title}>
+                        ${this.title}
+                    </h5>
+                    <p ?hidden=${!this.text}>${this.text}</p>
+                    <ul ?hidden=${!this.listItems.length}>
+                        ${this.listItems.map(
+                            (listItem) => html`<li>${listItem}</li>`
+                        )}
+                    </ul>
+                    <div
+                        class="card-actions justify-end"
+                        ?hidden=${!this.links.length}
+                    >
+                        ${this.links.length
+                            ? this.links.map(
+                                  (link) =>
+                                      html`<a href=${link.href}
+                                          >${link.text}</a
+                                      >`
+                              )
+                            : nothing}
+                    </div>
+                </div>
+            </div>
+        `;
+    }
 }
