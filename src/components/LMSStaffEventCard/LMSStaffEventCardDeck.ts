dcc8cd70eb11f6726e221e2c1fd09562919c08ef
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-06-08 15:35:08 +0200

    Rough refactor of staff event card deck w/ tailwind

--- src/components/LMSStaffEventCard/LMSStaffEventCardDeck.ts
+++ src/components/LMSStaffEventCard/LMSStaffEventCardDeck.ts
@@ -1,5 +1,4 @@
-import { bootstrapStyles } from "@granite-elements/granite-lit-bootstrap/granite-lit-bootstrap-min.js";
-import { LitElement, css, html } from "lit";
+import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
 import { map } from "lit/directives/map.js";
@@ -7,18 +6,19 @@ import { searchSyntax } from "../../docs/searchSyntax";
 import { InputConverter, TemplateResultConverter } from "../../lib/converters";
 import { __ } from "../../lib/translate";
 import {
-  Column,
-  LMSEvent,
-  LMSEventType,
-  LMSLocation,
-  LMSTargetGroup,
-  SortableColumns,
-  TaggedColumn,
-  TaggedData,
+    Column,
+    LMSEvent,
+    LMSEventType,
+    LMSLocation,
+    LMSTargetGroup,
+    SortableColumns,
+    TaggedColumn,
+    TaggedData,
 } from "../../sharedDeclarations";
 import { cardDeckStylesStaff } from "../../styles/cardDeck";
 import { skeletonStyles } from "../../styles/skeleton";
 import { utilityStyles } from "../../styles/utilities";
+import { tailwindStyles } from "../../tailwind.lit";
 import LMSAnchor from "../LMSAnchor";
 import LMSSearch from "../LMSSearch";
 import LMSStaffEventCardAttendees from "./LMSStaffEventCardAttendees";
@@ -27,282 +27,268 @@ import LMSStaffEventCardPreview from "./LMSStaffEventCardPreview";
 import LMSStaffEventsFilter from "./LMSStaffEventsFilter";
 
 declare global {
-  interface HTMLElementTagNameMap {
-    "lms-staff-event-card-form": LMSStaffEventCardForm;
-    "lms-staff-event-card-attendees": LMSStaffEventCardAttendees;
-    "lms-staff-event-card-preview": LMSStaffEventCardPreview;
-    "lms-staff-events-filter": LMSStaffEventsFilter;
-    "lms-anchor": LMSAnchor;
-    "lms-search": LMSSearch;
-  }
+    interface HTMLElementTagNameMap {
+        "lms-staff-event-card-form": LMSStaffEventCardForm;
+        "lms-staff-event-card-attendees": LMSStaffEventCardAttendees;
+        "lms-staff-event-card-preview": LMSStaffEventCardPreview;
+        "lms-staff-events-filter": LMSStaffEventsFilter;
+        "lms-anchor": LMSAnchor;
+        "lms-search": LMSSearch;
+    }
 }
 
 @customElement("lms-staff-event-card-deck")
 export default class LMSStaffEventCardDeck extends LitElement {
-  @property({ type: Array }) events: LMSEvent[] = [];
+    @property({ type: Array }) events: LMSEvent[] = [];
 
-  @property({ type: Array }) event_types: LMSEventType[] = [];
+    @property({ type: Array }) event_types: LMSEventType[] = [];
 
-  @property({ type: Array }) target_groups: LMSTargetGroup[] = [];
+    @property({ type: Array }) target_groups: LMSTargetGroup[] = [];
 
-  @property({ type: Array }) locations: LMSLocation[] = [];
+    @property({ type: Array }) locations: LMSLocation[] = [];
 
-  @property({ type: Array }) nextPage: Column[] | undefined = undefined;
+    @property({ type: Array }) nextPage: Column[] | undefined = undefined;
 
-  @property({ type: Boolean }) hasNoResults = false;
+    @property({ type: Boolean }) hasNoResults = false;
 
-  @property({ type: Number }) _page = 1;
+    @property({ type: Number }) _page = 1;
 
-  @property({ type: Number }) _per_page = 20;
+    @property({ type: Number }) _per_page = 20;
 
-  private data: TaggedColumn[] = [];
+    private data: TaggedColumn[] = [];
 
-  private cardStates: Map<string, string[]> = new Map();
+    private cardStates: Map<string, string[]> = new Map();
 
-  private inputConverter = new InputConverter();
+    private inputConverter = new InputConverter();
 
-  private sortableColumns: SortableColumns = ["id"];
+    private sortableColumns: SortableColumns = ["id"];
 
-  static override styles = [
-    bootstrapStyles,
-    skeletonStyles,
-    utilityStyles,
-    cardDeckStylesStaff,
-    css`
-      @media (max-width: 360px) {
-        lms-pagination {
-          font-size: 1rem;
-        }
-      }
-
-      @media (max-width: 576px) {
-        lms-search {
-          width: 100%;
-          margin: 1rem 0;
-        }
-
-        lms-pagination {
-          width: 100%;
-        }
-      }
-
-      .card-title-img {
-        background-size: cover;
-        background-position: center;
-        height: 5rem;
-        border-radius: 0.25rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    `,
-  ];
-
-  constructor() {
-    super();
-    this.sortableColumns = [
-      ...(this.sortableColumns as ["id"]),
-      "name",
-      "event_type",
-      "min_age",
-      "max_age",
-      "max_participants",
-      "start_time",
-      "end_time",
-      "registration_start",
-      "registration_end",
-      "location",
-      "image",
-      "status",
-      "registration_link",
-      "open_registration",
-      "description",
+    static override styles = [
+        tailwindStyles,
+        skeletonStyles,
+        utilityStyles,
+        cardDeckStylesStaff,
     ];
-  }
 
-  override connectedCallback() {
-    super.connectedCallback();
-    this.hydrate();
-  }
-
-  protected *getColumnData(query: LMSEvent, data?: TaggedData[]) {
-    for (const [name, value] of Object.entries(query)) {
-      yield [name, this.inputConverter.getInputTemplate({ name, value, data })];
+    constructor() {
+        super();
+        this.sortableColumns = [
+            ...(this.sortableColumns as ["id"]),
+            "name",
+            "event_type",
+            "min_age",
+            "max_age",
+            "max_participants",
+            "start_time",
+            "end_time",
+            "registration_start",
+            "registration_end",
+            "location",
+            "image",
+            "status",
+            "registration_link",
+            "open_registration",
+            "description",
+        ];
     }
-  }
 
-  private hydrate() {
-    /** Here we initialize the card states so we can track them
-     *  individually going forward. */
-    this.events.forEach(() => {
-      this.cardStates.set(
-        crypto.getRandomValues(new Uint32Array(2)).join("-"),
-        ["data"]
-      );
-    });
-
-    const data = this.events.map((event: LMSEvent) => {
-      return Object.fromEntries(
-        this.getColumnData(event, [
-          ["target_groups", this.target_groups],
-          ["location", this.locations],
-          ["event_type", this.event_types],
-        ])
-      );
-    });
-
-    /** Here we tag every datum with the uuid we generated earlier. */
-    this.data = data.map((datum, index) => {
-      const [uuid] = [...this.cardStates][index];
-      return {
-        ...datum,
-        uuid,
-      };
-    });
-  }
-
-  override updated(changedProperties: Map<string, never>) {
-    super.updated(changedProperties);
-    if (changedProperties.has("events")) {
-      this.hydrate();
-      this.requestUpdate();
+    override connectedCallback() {
+        super.connectedCallback();
+        this.hydrate();
     }
-  }
 
-  private handleTabClick(event: Event) {
-    event.preventDefault();
-    const target = event.target as HTMLElement;
-    const tab = target.closest("li");
-    if (!tab) return;
+    protected *getColumnData(query: LMSEvent, data?: TaggedData[]) {
+        for (const [name, value] of Object.entries(query)) {
+            yield [
+                name,
+                this.inputConverter.getInputTemplate({ name, value, data }),
+            ];
+        }
+    }
 
-    const previousActiveTab = tab.closest("ul")?.querySelector(".active");
-    if (!previousActiveTab) return;
-    previousActiveTab.classList.remove("active");
+    private hydrate() {
+        /** Here we initialize the card states so we can track them
+         *  individually going forward. */
+        this.events.forEach(() => {
+            this.cardStates.set(
+                crypto.getRandomValues(new Uint32Array(2)).join("-"),
+                ["data"]
+            );
+        });
 
-    tab.firstElementChild?.classList.add("active");
+        const data = this.events.map((event: LMSEvent) => {
+            return Object.fromEntries(
+                this.getColumnData(event, [
+                    ["target_groups", this.target_groups],
+                    ["location", this.locations],
+                    ["event_type", this.event_types],
+                ])
+            );
+        });
 
-    const { content, uuid } = tab.dataset;
-    if (!content || !uuid) return;
-    this.cardStates.set(uuid, [content]);
-    this.requestUpdate();
-  }
+        /** Here we tag every datum with the uuid we generated earlier. */
+        this.data = data.map((datum, index) => {
+            const [uuid] = [...this.cardStates][index];
+            return {
+                ...datum,
+                uuid,
+            };
+        });
+    }
 
-  private handleSearch(e: CustomEvent) {
-    const { detail } = e;
-    this.dispatchEvent(
-      new CustomEvent("search", {
-        detail,
-        composed: true,
-        bubbles: true,
-      })
-    );
-  }
+    override updated(changedProperties: Map<string, never>) {
+        super.updated(changedProperties);
+        if (changedProperties.has("events")) {
+            this.hydrate();
+            this.requestUpdate();
+        }
+    }
 
-  private toggleDoc(e: MouseEvent) {
-    const target = e.target as HTMLElement;
-    const doc = target.nextElementSibling;
-    if (!doc) return;
+    private handleTabClick(event: Event) {
+        event.preventDefault();
+        const tab = event.target as HTMLElement;
+        if (!tab) return;
 
-    doc.classList.toggle("d-none");
-  }
+        const previousActiveTab = tab
+            .closest("div")
+            ?.querySelector(".tab-active");
+        if (!previousActiveTab) return;
+        previousActiveTab.classList.remove("tab-active");
 
-  override render() {
-    return html`
-      <div class="container-fluid mx-0">
-        <lms-staff-events-filter
-          .sortableColumns=${this.sortableColumns}
-          .event_types=${this.event_types}
-          .target_groups=${this.target_groups}
-          .locations=${this.locations}
-        >
-          <lms-search
-            @search=${this.handleSearch}
-            .sortableColumns=${this.sortableColumns}
-          ></lms-search>
-          <lms-pagination
-            .nextPage=${this.nextPage}
-            ._page=${this._page}
-            ._per_page=${this._per_page}
-          ></lms-pagination>
-        </lms-staff-events-filter>
-        <div
-          class="alert alert-info text-center ${classMap({
-            "d-none": !this.hasNoResults,
-          })}"
-          role="alert"
-        >
-          <h4 class="alert-heading">${__("No matches found")}.</h4>
-          <p>${__("Try refining your search.")}</p>
-          <button class="btn btn-outline-info" @click=${this.toggleDoc}>
-            ${__("Help")}
-          </button>
-          <div class="text-left d-none">
-            <hr />
-            ${searchSyntax}
-          </div>
-        </div>
-        <div class="card-deck">
-          ${map(this.data, (datum) => {
-            const { name, image, uuid } = datum;
-            const [title] = new TemplateResultConverter(name).getRenderValues();
-            const [src] = new TemplateResultConverter(image).getRenderValues();
-            const [state] = this.cardStates.get(uuid) || "data";
-            return html`
-              <div class="card mt-5">
-                <div class="card-header">
-                  <ul class="nav nav-tabs card-header-tabs">
-                    <li
-                      class="nav-item"
-                      data-content="data"
-                      data-uuid=${datum.uuid}
-                      @click=${this.handleTabClick}
+        tab.classList.add("tab-active");
+
+        const { content, uuid } = tab.dataset;
+        if (!content || !uuid) return;
+        this.cardStates.set(uuid, [content]);
+        this.requestUpdate();
+    }
+
+    private handleSearch(e: CustomEvent) {
+        const { detail } = e;
+        this.dispatchEvent(
+            new CustomEvent("search", {
+                detail,
+                composed: true,
+                bubbles: true,
+            })
+        );
+    }
+
+    private toggleDoc(e: MouseEvent) {
+        const target = e.target as HTMLElement;
+        const doc = target.nextElementSibling;
+        if (!doc) return;
+
+        doc.classList.toggle("hidden");
+    }
+
+    override render() {
+        return html`
+            <div class="mx-4">
+                <lms-staff-events-filter
+                    .sortableColumns=${this.sortableColumns}
+                    .event_types=${this.event_types}
+                    .target_groups=${this.target_groups}
+                    .locations=${this.locations}
+                >
+                    <lms-search
+                        slot="navbar-center"
+                        @search=${this.handleSearch}
+                        .sortableColumns=${this.sortableColumns}
+                    ></lms-search>
+                    <lms-pagination
+                        slot="navbar-end"
+                        .nextPage=${this.nextPage}
+                        ._page=${this._page}
+                        ._per_page=${this._per_page}
+                    ></lms-pagination>
+                </lms-staff-events-filter>
+                <div
+                    class="${classMap({
+                        hidden: !this.hasNoResults,
+                    })} alert alert-info text-center"
+                    role="alert"
+                >
+                    <h4>${__("No matches found")}.</h4>
+                    <p>${__("Try refining your search.")}</p>
+                    <button
+                        class="btn-info btn-outline btn"
+                        @click=${this.toggleDoc}
                     >
-                      <a class="nav-link active" href="#">${__("Data")}</a>
-                    </li>
-                    <li
-                      class="nav-item"
-                      data-content="attendees"
-                      data-uuid=${datum.uuid}
-                      @click=${this.handleTabClick}
-                    >
-                      <a class="nav-link" href="#">${__("Waitlist")}</a>
-                    </li>
-                    <li
-                      class="nav-item"
-                      data-content="preview"
-                      data-uuid=${datum.uuid}
-                      @click=${this.handleTabClick}
-                    >
-                      <a class="nav-link">${__("Preview")}</a>
-                    </li>
-                  </ul>
+                        ${__("Help")}
+                    </button>
+                    <div class="hidden text-left">
+                        <hr />
+                        ${searchSyntax}
+                    </div>
                 </div>
-                <div class="card-body">
-                  <div
-                    class="card-title card-title-img"
-                    style="background-image: url(${src});"
-                  >
-                    <h3>
-                      <span class="badge badge-light">${title}</span>
-                    </h3>
-                  </div>
-                  <lms-staff-event-card-form
-                    .datum=${datum}
-                    ?hidden=${!(state === "data")}
-                  ></lms-staff-event-card-form>
-                  <lms-staff-event-card-attendees
-                    ?hidden=${!(state === "attendees")}
-                  ></lms-staff-event-card-attendees>
-                  <lms-staff-event-card-preview
-                    ?hidden=${!(state === "preview")}
-                    .datum=${datum}
-                  ></lms-staff-event-card-preview>
+                <div class="card-deck">
+                    ${map(this.data, (datum) => {
+                        const { name, image, uuid } = datum;
+                        const [title] = new TemplateResultConverter(
+                            name
+                        ).getRenderValues();
+                        const [src] = new TemplateResultConverter(
+                            image
+                        ).getRenderValues();
+                        const [state] = this.cardStates.get(uuid) || "data";
+                        return html`
+                            <div class="card bg-white shadow-md">
+                                <div>
+                                    <div class="tabs w-full">
+                                        <a
+                                            class="tab-bordered tab tab-active tab-lg flex-auto text-base"
+                                            data-content="data"
+                                            data-uuid=${datum.uuid}
+                                            @click=${this.handleTabClick}
+                                            >${__("Data")}</a
+                                        >
+                                        <a
+                                            class="tab-bordered tab tab-lg flex-auto text-base"
+                                            data-content="attendees"
+                                            data-uuid=${datum.uuid}
+                                            @click=${this.handleTabClick}
+                                        >
+                                            ${__("Waitlist")}</a
+                                        >
+                                        <a
+                                            class="tab-bordered tab tab-lg flex-auto text-base"
+                                            data-content="preview"
+                                            data-uuid=${datum.uuid}
+                                            @click=${this.handleTabClick}
+                                        >
+                                            ${__("Preview")}</a
+                                        >
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div
+                                        class="card-title flex h-24 items-center justify-center rounded-md bg-cover bg-center"
+                                        style="background-image: url(${src});"
+                                    >
+                                        <h3
+                                            class="rounded-lg bg-white p-2 text-3xl"
+                                        >
+                                            ${title}
+                                        </h3>
+                                    </div>
+                                    <lms-staff-event-card-form
+                                        .datum=${datum}
+                                        ?hidden=${!(state === "data")}
+                                    ></lms-staff-event-card-form>
+                                    <lms-staff-event-card-attendees
+                                        ?hidden=${!(state === "attendees")}
+                                    ></lms-staff-event-card-attendees>
+                                    <lms-staff-event-card-preview
+                                        ?hidden=${!(state === "preview")}
+                                        .datum=${datum}
+                                    ></lms-staff-event-card-preview>
+                                </div>
+                            </div>
+                        `;
+                    })}
                 </div>
-              </div>
-            `;
-          })}
-        </div>
-      </div>
-    `;
-  }
+            </div>
+        `;
+    }
 }
