778b425e34d5d3f7aba851988b9437331811c571
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-06-08 15:35:20 +0200

    Update import for tailwind

--- src/components/LMSStaffEventCard/LMSStaffEventCardAttendees.ts
+++ src/components/LMSStaffEventCard/LMSStaffEventCardAttendees.ts
@@ -1,28 +1,29 @@
-import { bootstrapStyles } from "@granite-elements/granite-lit-bootstrap/granite-lit-bootstrap-min.js";
-import { LitElement, css, html } from "lit";
+import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 import { __ } from "../../lib/translate";
 import { skeletonStyles } from "../../styles/skeleton";
+import { tailwindStyles } from "../../tailwind.lit";
+
 @customElement("lms-staff-event-card-attendees")
 export default class LMSStaffEventCardAttendees extends LitElement {
-  static override styles = [
-    bootstrapStyles,
-    skeletonStyles,
-    css`
-      svg {
-        display: inline-block;
-        width: 1em;
-        height: 1em;
-        color: #ffffff;
-      }
+    static override styles = [
+        tailwindStyles,
+        skeletonStyles,
+        css`
+            svg {
+                display: inline-block;
+                width: 1em;
+                height: 1em;
+                color: #ffffff;
+            }
 
-      button {
-        white-space: nowrap;
-      }
-    `,
-  ];
+            button {
+                white-space: nowrap;
+            }
+        `,
+    ];
 
-  override render() {
-    return html` <h1 class="text-center">${__("Not implemented")}!</h1> `;
-  }
+    override render() {
+        return html` <h1 class="text-center">${__("Not implemented")}!</h1> `;
+    }
 }
