0bbc0c5127a4b064ae2adc6c11809bd565ef07ce
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-06-08 15:33:37 +0200

    Housekeeping

---
+++ src/components/LMSView.ts
@@ -0,0 +1,12 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { tailwindStyles } from "../tailwind.lit";
+
+@customElement("lms-view")
+export default class LMSView extends LitElement {
+  static override styles = [tailwindStyles];
+
+  override render() {
+    return html`<slot></slot>`;
+  }
+}
