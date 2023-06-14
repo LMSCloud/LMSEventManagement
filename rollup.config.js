59b59a3f752595304d8fe109e5d79b36b80dcc70
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-06-14 18:45:15 +0200

    Remove postcss plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
-import autoprefixer from "autoprefixer";
-import path from "path";
 import minifyHTML from "rollup-plugin-minify-html-literals";
-import postcss from "rollup-plugin-postcss";
 
 export default {
   input: "./src/main.ts",
@@ -18,18 +15,6 @@ export default {
   plugins: [
     nodeResolve(),
     commonjs(),
-    postcss({
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: ["last 2 versions", "ie >= 11"],
-        }),
-      ],
-      extract: path.resolve(
-        "Koha/Plugin/Com/LMSCloud/EventManagement/dist/main.css"
-      ),
-      minimize: true,
-      sourceMap: true,
-    }),    
     typescript({
       declaration: false,
       declarationMap: false,
