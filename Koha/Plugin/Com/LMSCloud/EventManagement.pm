31156346b0fbbcee475d86369ace8d13228159b6
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-07-13 14:16:49 +0200

    Reset version and date_updated to defaults specified in regex

--- Koha/Plugin/Com/LMSCloud/EventManagement.pm
+++ Koha/Plugin/Com/LMSCloud/EventManagement.pm
@@ -76,7 +76,7 @@ BEGIN {
 }
 
 ## Here we set our plugin version
-our $VERSION         = '1.0.0';
+our $VERSION         = "1.0.0";
 our $MINIMUM_VERSION = '18.05';
 
 ## Here is our metadata, some keys are required, some are optional
@@ -84,7 +84,7 @@ our $METADATA = {
     name            => 'LMSEventManagement',
     author          => 'LMSCloud GmbH',
     date_authored   => '2021-10-15',
-    date_updated    => '2023-05-17',
+    date_updated    => "1900-01-01",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
