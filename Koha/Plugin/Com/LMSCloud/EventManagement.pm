355bed527e29ac1db7cc314ce3829f09599c8387
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-08-07 09:47:30 +0200

    follow-up: increment patch version after fix

--- Koha/Plugin/Com/LMSCloud/EventManagement.pm
+++ Koha/Plugin/Com/LMSCloud/EventManagement.pm
@@ -77,7 +77,7 @@ BEGIN {
 }
 
 ## Here we set our plugin version
-our $VERSION         = '1.3.11';
+our $VERSION         = '1.3.12';
 our $MINIMUM_VERSION = '18.05';
 
 ## Here is our metadata, some keys are required, some are optional
@@ -85,7 +85,7 @@ our $METADATA = {
     name            => 'LMSEventManagement',
     author          => 'LMSCloud GmbH',
     date_authored   => '2021-10-15',
-    date_updated    => '2023-07-20',
+    date_updated    => '2023-08-07',
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
