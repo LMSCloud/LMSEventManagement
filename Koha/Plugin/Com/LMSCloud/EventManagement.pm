136b0568f080fe23e655c3bd39b76f5deaccf9c1
pders01 <paulderscheid@gmail.copm>
2023-10-28 21:14:10 +0200

    follow-up: increment version on patch

--- Koha/Plugin/Com/LMSCloud/EventManagement.pm
+++ Koha/Plugin/Com/LMSCloud/EventManagement.pm
@@ -77,7 +77,7 @@ BEGIN {
 }
 
 ## Here we set our plugin version
-our $VERSION         = '1.4.2';
+our $VERSION         = '1.4.3';
 our $MINIMUM_VERSION = '18.05';
 
 ## Here is our metadata, some keys are required, some are optional
