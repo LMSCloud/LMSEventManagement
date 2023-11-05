c0704ca1abb528d66fc9d893d9ee4608b23f0613
pders01 <paulderscheid@gmail.copm>
2023-11-05 17:10:57 +0100

    chore: add caching for perltidy (perltidyc)
    - Add cache files for prettier and perltidy to gitignore
    - Modify format package script to use caches for prettier and perltidy

--- Koha/Plugin/Com/LMSCloud/EventManagement/Opac/events.pl
+++ Koha/Plugin/Com/LMSCloud/EventManagement/Opac/events.pl
@@ -23,7 +23,7 @@ use English qw(-no_match_vars);
 
 use C4::Auth qw( get_template_and_user );
 use C4::Context;
-use C4::Output qw( output_html_with_http_headers );
+use C4::Output    qw( output_html_with_http_headers );
 use C4::Languages qw( getlanguage );
 
 use CGI qw ( -utf8 );
