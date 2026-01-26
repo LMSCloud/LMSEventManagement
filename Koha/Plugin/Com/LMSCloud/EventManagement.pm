ea60b8cf8316366221722efcae2f5d48e1c0ced5
Paul Derscheid <paul.derscheid@lmscloud.de>
2026-01-26 11:58:56 +0100

    chore: increment minor version

--- Koha/Plugin/Com/LMSCloud/EventManagement.pm
+++ Koha/Plugin/Com/LMSCloud/EventManagement.pm
@@ -68,7 +68,7 @@ BEGIN {
 }
 
 ## Here we set our plugin version
-our $VERSION         = '2.9.4';
+our $VERSION         = '2.10.0';
 our $MINIMUM_VERSION = '22.11';
 
 ## Here is our metadata, some keys are required, some are optional
@@ -76,7 +76,7 @@ our $METADATA = {
     name            => 'LMSEventManagement',
     author          => 'LMSCloud GmbH',
     date_authored   => '2021-10-15',
-    date_updated    => '2026-01-08',
+    date_updated    => '2026-01-26',
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
